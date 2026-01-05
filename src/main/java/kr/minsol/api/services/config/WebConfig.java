827f08ef361347854d892e995e3213e2370e5265
minsol0302 <pjjw1233@gmail.com>
2026-01-05 17:59:15 +0900

    프론트엔드와 연결

--- src/main/java/kr/minsol/api/services/config/WebConfig.java
+++ src/main/java/kr/minsol/api/services/config/WebConfig.java
@@ -10,7 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://127.0.0.1:3000",
+                        "https://www.minsol.kr",
+                        "https://minsol.kr")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
